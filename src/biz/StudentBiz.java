eeca23587dac8a868c3d47e7537b5f59300fc815
12613 <1261337209@qq.com>
2020-09-16 21:01:09 +0800

    学生登录

--- src/biz/StudentBiz.java
+++ src/biz/StudentBiz.java
@@ -1,5 +1,11 @@
 package biz;
 
+import Util.sql.DBHelper;
+import beain.Student;
+
+import java.util.List;
+import java.util.Map;
+
 public class StudentBiz {
     // 更改密码
     public void modifyPassword(String newPwd) {
@@ -18,7 +24,30 @@ public class StudentBiz {
     }
 
     // 验证账号密码合法性然后登陆
-    public void login(String Sno, String Spw) {
+    public boolean login(String Sno, String Spw) throws BizException {
+        if(Sno==null ||Sno.trim().isEmpty() ) {
+            throw new BizException("请输入用户名 ! ");
+        }
+
+        if(Spw==null || Spw.trim().isEmpty() ) {
+            throw new BizException("请输入密码! ");
+        }
+
+        // 需要基本的判断
+        String sql = "select * from student where Sno=? and Spw=?";
+        List<Map<String, Object>> list= new DBHelper().query(sql, Sno,Spw);
+
+        if(list.size() == 1) {
+            return true;
+        } else {
+            throw new BizException("用户名或密码错误 ! ");
+        }
 
     }
+
+    public static void main(String[] args) throws BizException {
+        StudentBiz studentBiz = new StudentBiz();
+        System.out.println(studentBiz.login("1001","1234"));
+    }
+
 }
