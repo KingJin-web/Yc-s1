7bca6f39eb2394bb49b60922cabe8cc416ca14ab
12613 <1261337209@qq.com>
2020-09-20 14:18:28 +0800

    修改学生密码

---
+++ src/biz/TeacherBiz.java
@@ -0,0 +1,52 @@
+package biz;
+
+import Util.sql.DBHelper;
+import beain.Teacher;
+
+import java.util.List;
+import java.util.Map;
+
+public class TeacherBiz {
+    public void TeacherDelete(String stuName) throws BizException{
+        if (stuName == null || stuName.trim().isEmpty() ){
+            throw new BizException("请输入完整的学生姓名！");
+        }
+        DBHelper dbh = new DBHelper();
+        String sql = "delete from student where Sname = ?";
+        dbh.update(sql,stuName);
+
+    }
+    public void selectByName(String stuName) throws BizException{
+        if (stuName == null || stuName.trim().isEmpty() ){
+            throw new BizException("请输入完整的学生姓名！");
+        }
+
+        DBHelper dbh = new DBHelper(false);
+
+        String sql = "select * from student where Sname = ?";
+        dbh.query(sql,stuName);
+        List<Map<String, Object>> list = dbh.query(sql,stuName);
+        for (Map<String, Object> map : list) {
+            System.out.println(map);
+        }
+    }
+    public boolean login(String tno,String tpw) throws BizException {
+        if (tno == null || tno.trim().isEmpty() ){
+            throw new BizException("请输入用户名！");
+        }
+
+        if (tpw == null || tpw.trim().isEmpty() ){
+            throw new BizException("请输入密码！");
+        }
+
+        String sql = "select * from teacher where tno = ? and tpw = ?";
+        List<Teacher> list = new DBHelper().query(sql,Teacher.class,tno,tpw);
+        if (list.size() == 1){
+            return true;
+        }else {
+            throw new BizException("用户名或密码输入错误!");
+        }
+    }
+
+
+}
